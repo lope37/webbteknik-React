a94544e54d032d33ae793fb8b26c0b3b14fdd04e
lope37 <r.love.pettersson@gmail.com>
2026-08-21 16:09:38 +0200

    + styles.css

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/styles.css';
 
 class AboutPage extends Component {
     render() {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/styles.css';
 
 class HomePage extends Component {
     render() {
